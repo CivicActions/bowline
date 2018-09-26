98206c6ed81ec211adf97dc341fcc9b1e12b2497
David Numan <david.numan@civicactions.com>
2018-09-26 13:42:07 -0400

    New branch for 2.x

--- .docker/devtools/devtools.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Dev Tools with nodejs.\n');
-}).listen(80, '0.0.0.0');
-console.log('Server running at http://127.0.0.1:80/');
